1f6af8b2efecc1701408bfad2014696267031899
lechat <34833312+maoqifan1@users.noreply.github.com>
2019-10-08 13:27:40 +0800

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,5 +1,3 @@
-from time import sleep
-import numpy as np
 
 def qSort(arr):
     if len(arr) < 2:
@@ -10,16 +8,7 @@ def qSort(arr):
         more = [i for i in arr[1:] if i > pivot]
         return  qSort(less) + [pivot] + qSort(more)
 
-def form(arr):
-    arr1 = arr[:]
-    arr2 = []
-    for i in arr:
-        k = 0
-        for j in arr1:
-            k = k + (i * j)
-        arr2.append(k)
-    return arr2
-    
+
 if __name__ == "__main__":
     print(qSort([2, 1, 3, 5, 8, 10, 7]))
 
